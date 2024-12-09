2f79ad817f8c2e9b029459e7b73df33b33cc4c2d
Stephen Badcock <badcockstephen73@gmail.com>
2024-12-09 10:52:55 -0330

    home page test
    putting background and images

--- FitFab/src/pages/Home.jsx
+++ FitFab/src/pages/Home.jsx
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="home-container">
-      <h1>this will be the home page</h1>
       <div className="categories">
         <Link to="/menswear" className="category-link">
-         
-          <h2>Men's Wear</h2>
-        </Link>
+        <img 
+          src="/images/products/mens_leather_jacket.jpg"
+          alt="Menswear"/>        </Link>
         <Link to="/womenswear" className="category-link">
-          <h2>Women's Wear</h2>
-        </Link>
+        <img 
+          src="/images/products/womens_trenchcoat.jpg"
+          alt="Womenswear"/>        </Link>
         <Link to="/kidswear" className="category-link">
-          <h2>Kids' Wear</h2>
-        </Link>
+        <img 
+          src="/images/products/kids_fleece_jacket.jpg"
+          alt="Kidswear"/>        </Link>
       </div>
     </div>
   );
